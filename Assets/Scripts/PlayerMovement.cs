54061e28ec9b215ac794aeb8bea1995116361e64
Brandon <github@doxle.net>
2023-04-23 13:30:27 -0400

    fixed movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,19 +11,19 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetKey("d"))
         {
-            transform.Translate(new Vector3(.01F*Time.deltaTime, 0, 0));
+            transform.Translate(new Vector3(.01F, 0, 0));
         }
         if (Input.GetKey("a"))
         {
-            transform.Translate(new Vector3(-.01F*Time.deltaTime, 0, 0));
+            transform.Translate(new Vector3(-.01F, 0, 0));
         }
         if (Input.GetKey("w"))
         {
-            transform.Translate(new Vector3(0, .01F*Time.deltaTime, 0));
+            transform.Translate(new Vector3(0, .01F, 0));
         }
         if (Input.GetKey("s"))
         {
-            transform.Translate(new Vector3(0, -.01F*Time.deltaTime, 0));
+            transform.Translate(new Vector3(0, -.01F, 0));
         }
     }
 }
