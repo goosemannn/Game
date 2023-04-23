ae5bda2db92cc3e1195ce544d0e0eb3cce5c701c
JuneBugU <jman7861276@gmail.com>
2023-04-23 17:44:33 -0400

    More features added to start menu
    Read the title

--- Assets/Fucking femboy menu scripts/MenueCOde.cs
+++ Assets/Fucking femboy menu scripts/MenueCOde.cs
@@ -8,29 +8,29 @@ public class MenueCOde : MonoBehaviour
 {
     public InputField userInput1;
     public string userName;
+    public Dropdown Pronowns;
+    public int pronownvar;
+    public Dropdown Level;
+    public int levelvarl;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-    
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     public void OnSubmitCLick()
     {
         userName = userInput1.text;
-        if(userName != "0")
-        { 
+        if (userName != "0")
+        {
             userName = userInput1.text;
             PlayerPrefs.SetString("userName", userName);
+            pronownvar = Pronowns.value + 1;
+            PlayerPrefs.SetInt("Pronowns",pronownvar);
+            levelvarl = Level.value + 1;
+            PlayerPrefs.SetInt("dificulty", levelvarl);
             SceneManager.LoadScene("SampleScene");
         }
     }
-
 }
