098f656417c57c274b9e220aa4e2bf5bb93ab00b
JuneBugU <jman7861276@gmail.com>
2023-04-23 15:24:59 -0400

    Starter menue scene and code
    Yo why does this have meta data in it?
    idk man i dont know how to computer

---
+++ Assets/Fucking femboy menu scripts/MenueCOde.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenueCOde : MonoBehaviour
+{
+    public InputField userInput1;
+    public string userName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        userInput1.onEndEdit.AddListener(delegate { OnEndEdit(userInput1); });
+    }
+    private void OnEndEdit(InputField input)
+    {
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            OnSubmitCLick();
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnSubmitCLick()
+    {
+        userName = userInput1.text;
+        if(userName != "0")
+        { 
+            userName = userInput1.text;
+            PlayerPrefs.SetString("userName", userName);
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+
+}
